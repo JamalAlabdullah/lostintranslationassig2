52c9a03084534aec8ffd4a527341d52d88bc02cb
Harald Kvisli <harald.kvisli@no.experis.com>
2022-09-20 09:13:06 +0200

    style: prettify code in TranslationForm.jsx.

--- src/components/Translation/TranslationForm.jsx
+++ src/components/Translation/TranslationForm.jsx
@@ -6,29 +6,25 @@ const TranslationForm = () => {
     return (
         <>
             <div className="container">
-          <h3 className="welcomeMessage"> Welcome to translation page</h3>
-          <p className="enterLable">Enter the word or phrase you want to translate into a sign: </p>
+                <h3 className="welcomeMessage"> Welcome to translation page</h3>
+                <p className="enterLable"> Enter the word or phrase you want to translate into a sign: </p>
             
-             {/*translate felt */}
-          <div className="input-group mb-3">
-           <input type="text" className="form-control" placeholder="Type..."
-            />
-            <div className="input-group-append">
-                <button className="btn btn-outline-primary" type="button">Translate</button>
-            </div>
-          </div>
+                {/*translate felt */}
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Type..."/>
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-primary" type="button">Translate</button>
+                    </div>
+                </div>
 
-            {/*Results card */}
-            <div className="card">
-                <div className="card-header">The results for your translation</div>
-            <div className="card-body">
-                <p className="card-text">Here should the result of translation be shown</p>
+                {/*Results card */}
+                <div className="card">
+                    <div className="card-header">The results for your translation</div>
+                        <div className="card-body">
+                            <p className="card-text">Here should the result of translation be shown</p>
+                        </div>
+                </div>
             </div>
-            </div>
-
-            </div>
-
-
         </>
       
 
--- src/views/Translation.jsx
+++ src/views/Translation.jsx
@@ -1,4 +1,5 @@
 import TranslationForm from "../components/Translation/TranslationForm";
+import withAuth from "../hoc/withAuth";
 
 const Translation = () => {
 
