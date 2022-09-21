d148e04917209dc27ee6ae533322c57811fcc4ba
Harald Kvisli <harald.kvisli@no.experis.com>
2022-09-21 13:36:36 +0200

    feat: add functionality to log out

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,10 +1,18 @@
+import { STORAGE_KEY_USER } from "../../const/storageKey";
+import { useUser } from "../../context/UserContext";
+import { storageSave } from "../../utils/storage";
+import { useEffect } from "react";
+
 const Logout = () => {
+    const { user, setUser } = useUser()
 
-
-window.location='/';
-
+    if (window.confirm("Are you sure you want to log out?")) {
+        storageSave(STORAGE_KEY_USER, null)
+        setUser(null)
+        window.location = "/"
+        }
 }
 
 
-
+    
 export default Logout;
