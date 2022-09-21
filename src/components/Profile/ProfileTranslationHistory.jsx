9066ad31bc77a110467251a6140c86fb1d92dd8c
Harald Kvisli <harald.kvisli@no.experis.com>
2022-09-21 12:55:14 +0200

    feat: implemented functionality for deleting translation history.

--- src/components/Profile/ProfileTranslationHistory.jsx
+++ src/components/Profile/ProfileTranslationHistory.jsx
@@ -1,9 +1,35 @@
+import { translationClearHistory } from "../../api/translate"
 import ProfileTranslationHistoryItem from "./ProfileTranslationHistoryItem"
+import { useUser } from "../../context/UserContext";
+import { storageSave } from "../../utils/storage";
+import { STORAGE_KEY_USER } from "../../const/storageKey";
 
 const ProfileTranslationHistory = ({ translations }) => {
-    console.log(translations);
-    const translationList = translations.slice(0, 10).map(translation => <ProfileTranslationHistoryItem key={ translation } translation={ translation }/>)
-  
+    
+    const translationList = translations.slice(0, 10).map((translation, index) => <ProfileTranslationHistoryItem key={ index } translation={ translation }/>)
+    const { user, setUser } = useUser()
+
+    const handleDeleteTranslationsClick = async () => {
+        
+        if (!window.confirm("Are you sure you want to delete your history?")) {
+            return
+        }
+
+        const [ clearError ] = await translationClearHistory(user.id)
+
+        if (clearError !== null) {
+            return
+        }
+        
+        const updatedUser = {
+            ...user,
+            orders: []
+        }
+        
+        storageSave(STORAGE_KEY_USER, updatedUser)
+        setUser(updatedUser)
+    }
+
     return (
         <>
             <div className="card1">
@@ -16,7 +42,7 @@ const ProfileTranslationHistory = ({ translations }) => {
                     </div>
             </div>
             <div className="deleteBtn">
-                <button type="submit" className="btn btn-primary">Delete Translations</button>
+                <button type="submit" className="btn btn-primary" onClick={ handleDeleteTranslationsClick }>Delete Translations</button>
             </div>
         </>
     )
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
 import ProfileHeader from "../components/Profile/ProfileHeader";
 import ProfileTranslationHistory from "../components/Profile/ProfileTranslationHistory";
 import { useUser } from "../context/UserContext";
 import withAuth from "../hoc/withAuth";
+import { findUserById } from "../api/user";
+import { storageSave } from "../utils/storage";
+import { STORAGE_KEY_USER } from "../const/storageKey";
 
 
 const Profile = () => {
 
-    const { user } = useUser();
+    const { user, setUser } = useUser();
+
+    useEffect(() => {
+        const findUser = async () => {
+            const [ error, latestUser ] = await findUserById(user.id)
+            
+            if (error === null) {
+                storageSave(STORAGE_KEY_USER, latestUser)
+                setUser(latestUser)
+            }
+        }
+
+        findUser()
+    }, [user.translations])
+
     return (
         <section>
             <ProfileHeader username={ user.username } />
